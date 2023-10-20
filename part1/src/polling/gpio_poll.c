9582da7eb83cfa65847f0cb283a839e3a3b7636c
HafthorArni <111596842+HafthorArni@users.noreply.github.com>
2023-10-20 18:34:32 +0000

    Add files via upload
    gpio_read.c: Using sysfs from a C/C++ application polling the pin status using a timed read().
    gpio_poll.c: Using sysfs from a C/C++ application making use of the poll() function provided
    
    maybe need to tune timer in gpio_read and measure CPU load again

--- part1/src/polling/gpio_poll.c
+++ part1/src/polling/gpio_poll.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <poll.h>
 #include <stdio.h>
 
 int main() {
@@ -9,29 +10,42 @@ int main() {
     write(fd_export, "27", 2);
     close(fd_export);
 
-    // Set gpio17 as output and gpio27 as input
-    int fd_direction17 = open("/sys/class/gpio/gpio17/direction", O_WRONLY);
+    // Set gpio27 as input and gpio17 as output
     int fd_direction27 = open("/sys/class/gpio/gpio27/direction", O_WRONLY);
-    write(fd_direction17, "out", 3);
+    int fd_direction17 = open("/sys/class/gpio/gpio17/direction", O_WRONLY);
     write(fd_direction27, "in", 2);
-    close(fd_direction17);
+    write(fd_direction17, "out", 3);
     close(fd_direction27);
+    close(fd_direction17);
 
-    // Main loop to toggle gpio17 according to the value of gpio27
-    int counter = 0;
-    while (counter < 1000000) {
-        int fd_value27 = open("/sys/class/gpio/gpio27/value", O_RDONLY);
-        int fd_value17 = open("/sys/class/gpio/gpio17/value", O_WRONLY);
+    // Set gpio27 interrupt
+    int fd_edge = open("/sys/class/gpio/gpio27/edge", O_WRONLY);
+    write(fd_edge, "both", 4);  // Trigger on both rising and falling edges
+    close(fd_edge);
 
-        char value;
-        read(fd_value27, &value, 1);
-        write(fd_value17, &value, 1);
-
-        close(fd_value27);
-        close(fd_value17);
-
-        counter++;
+    struct pollfd pfd;
+    int fd_value27 = open("/sys/class/gpio/gpio27/value", O_RDONLY);
+    pfd.fd = fd_value27;
+    pfd.events = POLLPRI;  // We are interested in urgent data (edge triggered)
+    
+    for(int i=0; i<20000; i++) {
+        lseek(fd_value27, 0, SEEK_SET);  // Reset the file pointer due to the read
+        char buf[2] = {0};  // Initialize buffer to store the read value
+        read(fd_value27, buf, 2);  // Dummy read to clear the interrupt
+        
+        int ret = poll(&pfd, 1, -1);  // Wait indefinitely for an event
+        if(ret > 0) {
+            lseek(fd_value27, 0, SEEK_SET);  // Reset the file pointer for a new read
+            read(fd_value27, buf, 2);  // Read the new value
+            int fd_value17 = open("/sys/class/gpio/gpio17/value", O_WRONLY);
+            write(fd_value17, buf, 1);  // Copy the state to the output pin
+            close(fd_value17);
+        } else {
+            printf("Error polling gpio27.\n");
+        }
     }
+
+    close(fd_value27);
 
     // Disable gpio17 and gpio27
     int fd_unexport = open("/sys/class/gpio/unexport", O_WRONLY);
---
+++ part1/src/polling/gpio_read.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main() {
+    // Enable gpio17 and gpio27
+    int fd_export = open("/sys/class/gpio/export", O_WRONLY);
+    write(fd_export, "17", 2);
+    write(fd_export, "27", 2);
+    close(fd_export);
+
+    // Set gpio17 as output and gpio27 as input
+    int fd_direction17 = open("/sys/class/gpio/gpio17/direction", O_WRONLY);
+    int fd_direction27 = open("/sys/class/gpio/gpio27/direction", O_WRONLY);
+    write(fd_direction17, "out", 3);
+    write(fd_direction27, "in", 2);
+    close(fd_direction17);
+    close(fd_direction27);
+
+    // Main loop to toggle gpio17 according to the value of gpio27
+    int counter = 0;
+    while (counter < 10000) {
+        int fd_value27 = open("/sys/class/gpio/gpio27/value", O_RDONLY);
+        int fd_value17 = open("/sys/class/gpio/gpio17/value", O_WRONLY);
+
+        char value;
+        read(fd_value27, &value, 1);
+        write(fd_value17, &value, 1);
+
+        close(fd_value27);
+        close(fd_value17);
+
+        counter++;
+        usleep(10);
+    }
+
+
+    // Disable gpio17 and gpio27
+    int fd_unexport = open("/sys/class/gpio/unexport", O_WRONLY);
+    write(fd_unexport, "17", 2);
+    write(fd_unexport, "27", 2);
+    close(fd_unexport);
+
+    return 0;
+}
